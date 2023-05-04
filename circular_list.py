4dd1f44dd5f16fc93008ea801b830292ad928ba3
thanhvocam94 <thanhvocam94@gmail.com>
2023-05-04 17:38:21 +0700

    circular_list

---
+++ circular_list.py
@@ -0,0 +1,26 @@
+a = [1,2,3,4,5,6,7]
+b = [4,5,6,7,1,2,3]
+first_element_idx = None
+
+if len(a) != len(b):
+    print("Not circularly identical!")
+
+for i in range(len(a)):
+    if b[0] == a[i]:
+        first_element_idx = i
+
+comparison_results = []
+
+for i in range(first_element_idx, len(a)):
+    comparison_result = a[i]==b[i - first_element_idx]
+    comparison_results.append(comparison_result)
+
+for i in range(first_element_idx): 
+    comparison_result = a[i]==b[len(b) - first_element_idx + i]
+    comparison_results.append(comparison_result)
+
+if sum(comparison_results) == len(a):
+    print("Is circularly identical!")
+else:
+    print("Not circularly identical!")
+     
