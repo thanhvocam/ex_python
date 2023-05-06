8c4ba3c3e8872e81f55c877c40f85438bc006fb6
thanhvocam94 <thanhvocam94@gmail.com>
2023-05-06 15:55:09 +0700

    cach 2

--- circular_list.py
+++ circular_list.py
@@ -1,6 +1,7 @@
 a = [1,5,2,5,5,6,7]
-b = [5,2,5,5,6,7,1]
+b = [5,5,6,7,1,5,2]
 first_element_idx = None
+ls_first_element_idx = []
 
 if len(a) != len(b):
     print("Not circularly identical!")
@@ -8,8 +9,11 @@ if len(a) != len(b):
 for i in range(len(a)):
     if b[0] == a[i]:
         first_element_idx = i
-        break
+        ls_first_element_idx.append(first_element_idx)
+print(ls_first_element_idx)
+        
 comparison_results = []
+
 
 for i in range(first_element_idx, len(a)):
     comparison_result = a[i]==b[i - first_element_idx]
@@ -23,4 +27,4 @@ if sum(comparison_results) == len(a):
     print("Is circularly identical!")
 else:
     print("Not circularly identical!")
-     
+    
